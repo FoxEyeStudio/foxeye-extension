ecf0505552e1691bcc24c30423c61be45df75055
Gary <Gary@foxeyestudio.io>
2022-07-12 15:42:33 +0800

    post transaction information to server

--- src/background/RiskCenter.js
+++ src/background/RiskCenter.js
@@ -1,6 +1,7 @@
 
-
+export const RiskType_Safe = 0;
 export const RiskType_PhishingWebsite = 1;
+
 export const RiskType_MaliciousContract = 2; // to
 export const RiskType_MaliciousAddress = 3; // erc20 transfer to
 export const RiskType_ApproveEOA = 4;// erc20 approve to
@@ -8,6 +9,9 @@ export const RiskType_TransferToContract = 5;// erc20 transfer to
 export const RiskType_SwapHighRiskToken = 6; // buy token on uniswap-like dex
 export const RiskType_SwapMediumRiskToken = 7; // buy token on uniswap-like dex
 
+// const api_host = 'http://localhost:6699/v1/';
+const api_host = 'https://api.foxeye.io/v1/';
+const tag = 'RiskCenter: ';
 let instance = null;
 class RiskCenter {
     static get = () => {
@@ -17,7 +21,50 @@ class RiskCenter {
         return instance;
     }
 
-
+    /*
+    * method: "eth_sendTransaction"
+    * params: Array(1)
+    * 0:
+    * data: "0x095ea7b3000000000000000000000000d33bd0b0d4abc1c82d46595aa73bdb5e464f0c4600000000000000000000000000000000000000000000000000000000000003e8"
+    * from: ""
+    * gas: undefined
+    * gasPrice: undefined
+    * maxFeePerGas: "0x6cbaa11ac"
+    * maxPriorityFeePerGas: "0x3B9ACA00"
+    * to: "0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"
+    * */
+    async parseTransaction(chain_id, transaction) {
+        console.log(tag, chain_id, transaction);
+        const { method } = transaction;
+        if (method == 'eth_sendTransaction') {
+            try {
+                const { data, to, from } = transaction.params[0];
+                const options = {
+                    method: 'POST',
+                    body: JSON.stringify({
+                        chain_id, data, to, from
+                    }),
+                    headers: {
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                        'Connection': 'keep-alive'
+                    },
+                };
+                const url = `${api_host}parse_tx`;
+                let result = await fetch(url, options).then(ret => ret.json()).catch(err => {
+                    return undefined;
+                });
+                if (result && result.status == '1') {
+                    return result.result;
+                }
+            } catch (e) {
+                console.log(tag, chain_id, transaction, e);
+            }
+        }
+        return {
+            type: RiskType_Safe
+        }
+    }
 }
 
 
--- src/proxy/index.js
+++ src/proxy/index.js
@@ -1,37 +1,20 @@
 import React from 'react';
-const dictionary = {
-    '0x095ea7b3': 'approve'
-};
+import RiskCenter from "../background/RiskCenter";
+
+const tag = 'FoxeyeProxy: ';
 
 class FoxeyeProxy {
     constructor() {
         this.initProxy();
     }
 
-    parseProxy(contract) {
-        const notableActionList = ['approve'];
-        console.log('@cyh: contract = ', contract);
-        if (typeof contract.method !== 'undefined') {
-            if (contract.method === 'eth_sendTransaction') {
-                if (contract.params[0].data) {
-                    const methodName = dictionary[contract.params[0].data.substring(0, 10)];
-                    if (notableActionList.includes(methodName)) {
-                        return { result: true, action: methodName };
-                    }
-                }
-
-            }
-        }
-        return { result: false };
-    }
-
     initProxy() {
         const proxyThis = this;
         const proxyHandler = {
             async apply(target, thisArg, argArray) {
-                const contract = [...argArray][0];
-                const result = proxyThis.parseProxy(contract).result;
-                const actionName = proxyThis.parseProxy(contract).action;
+                const transaction = [...argArray][0];
+                const result = await RiskCenter.get().parseTransaction(Number(window.ethereum.chainId).toString(), transaction);
+                console.log(tag, result);
                 return target(...argArray);
             }
         };
@@ -39,12 +22,11 @@ class FoxeyeProxy {
         const proxyInterval = setInterval(insertProxy(), 1000);
         function insertProxy() {
             if (typeof window.ethereum !== 'undefined') {
-                console.log('@cyh: window.ethereum = ', window.ethereum);
                 const proxy = new Proxy(window.ethereum.request, proxyHandler);
                 window.ethereum.request = proxy;
                 clearInterval(proxyInterval);
             } else {
-                console.log('@cyh: No Found window.ethereum');
+                console.log(tag, '@cyh: No Found window.ethereum');
             }
         }
         setTimeout(() => { clearInterval(proxyInterval); }, 10000);
