43283a17e3ef8497e5b937c1341284fec4893dda
Gary <Gary@foxeyestudio.io>
2022-07-18 11:30:54 +0800

    bug fix: chainId error.

--- src/background/index.js
+++ src/background/index.js
@@ -3,8 +3,8 @@ import riskCenter from './RiskCenter'
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     chrome.tabs.query({ currentWindow: true, active: true }, async function (tabs) {
         if (request.foxeye_extension_action === 'foxeye_sendTransaction') {
-            const { chianId, params, args } = request;
-            const result = await riskCenter.parseTransaction(chianId, params, args);
+            const { chainId, params, args } = request;
+            const result = await riskCenter.parseTransaction(chainId, params, args);
             sendResponse(result);
         }
     })
