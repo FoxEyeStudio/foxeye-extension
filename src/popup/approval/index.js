86033614e862436999597acd33c7bfa84b68d881
Gary <Gary@foxeyestudio.io>
2022-08-25 14:36:23 +0800

    approval part2

--- src/popup/approval/index.js
+++ src/popup/approval/index.js
@@ -8,10 +8,15 @@ import aboutHoverIcon from "../../images/ic_about_hover.png";
 import backIcon from "../../images/ic_back.png";
 import backHoverIcon from "../../images/ic_back_hover.png";
 import ic_account from "../../images/ic_account.png";
+import ic_eth_tag from "../../images/ic_eth_tag.png";
+import ic_bsc_tag from "../../images/ic_bsc_tag.png";
+import ic_about_website from "../../images/ic_about_website.png";
+import ic_about_hover from "../../images/ic_about_hover.png";
 import Lottie from "react-lottie";
 import {LoadingJson} from "../../common/utils";
 import {ethers} from "ethers";
 import {BscId, EthereumId} from "../../background/RiskCenter";
+import BigNumber from "bignumber.js";
 
 function Approval() {
     const navigate = useNavigate();
@@ -21,6 +26,7 @@ function Approval() {
 
     useEffect(() => {
         if (!ethers.utils.isAddress(account)) {
+            setApprovals([]);
             setLoading(false);
             return;
         }
@@ -33,6 +39,35 @@ function Approval() {
             setLoading(false);
         });
     }, []);
+
+    const tagImage = chain_id => {
+        chain_id = parseInt(chain_id);
+        switch (chain_id) {
+            case EthereumId: return ic_eth_tag;
+            case BscId: return ic_bsc_tag;
+        }
+        return undefined;
+    };
+
+    const timeFormat = timestamp => {
+        const d = new Date(timestamp * 1000);
+        return `${d.getDate()}/${d.getMonth()+1}/${d.getFullYear()} ${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
+    };
+
+    const amountFormat = amount => {
+        if (amount == 'Unlimited') {
+            return amount;
+        }
+        try {
+            let b = new BigNumber(amount);
+            b = b.dp(2);
+            return b.toString(10);
+        } catch (e) {
+            return amount;
+        }
+        vv.approved_amount == 'Unlimited' ? 'Unlimited' : new BigNumber()
+    };
+
     const tokens = approvals.length;
     let contracts = 0;
     for (let i = 0; i < approvals.length; i++) {
@@ -56,7 +91,7 @@ function Approval() {
                 <span className="detection-text">Approvals Management</span>
             </div>
             {loading && (
-                <div className='flex-col' style={{width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', position: "relative"}}>
+                <div className='approval-loading-wrap' style={{width: '100%', height: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center', position: "relative"}}>
                     <Lottie options={{
                         loop: true,
                         autoplay: true,
@@ -71,12 +106,12 @@ function Approval() {
                     <div className='approval-scanning flex-col'>Scanning...</div>
                 </div>
             )}
-            {!loading && ethers.utils.isAddress(account) && (
+            {!loading && ethers.utils.isAddress(account) && approvals.length > 0 && (
                 <div className='approval-scroll-div'>
                     <div className='approval-top-wrap'>
                         <div className='approval-top-address-wrap'>
                             <img src={ic_account} className='approval-top-address-icon'/>
-                            <div className='approval-top-address'>{account.substring(0, 6) + '...' + account.substring(account.length-4)}</div>
+                            <div className='approval-top-address'>{account.substring(0, 8) + '...' + account.substring(account.length-6)}</div>
                         </div>
                         <div className='approval-top-data-wrap'>
                             <div className='approval-top-type-wrap'>
@@ -92,6 +127,59 @@ function Approval() {
                                 <div className='approval-top-type-value'>Chains</div>
                             </div>
                         </div>
+                    </div>
+                    <div className='approval-list-wrap'>
+                        <div className='approval-list-title'>List of Approved Tokens</div>
+                        <div className='approval-list-title-desc'>Approving unknown dapp leads to security risks. Please check regularly!</div>
+                        {approvals.map((v, i, a) => (
+                            <div>
+                                <div className='approval-list-line'/>
+                                <div className='approval-list-item-title-wrap'>
+                                    <div>
+                                        <img src={`https://relayer.gopocket.finance/api/v1/getImage/${v.chain_id}/${v.token_address}`} className='approval-list-item-icon'/>
+                                        <img src={tagImage(v.chain_id)} className='approval-list-item-tag'/>
+                                    </div>
+                                    <div className='approval-list-item-token-name'>{v.token_symbol}</div>
+                                    <div className='approval-list-item-spender-amount'>{v.approved_list.length} Approval(s)</div>
+                                </div>
+                                {v.approved_list.map((vv, ii, aa) => (
+                                    <div>
+                                        <div className='approval-list-item-spender-name'>{vv.address_info?.tag ? vv.address_info.tag : vv.address_info?.contract_name ? vv.address_info.contract_name : 'unknown'}</div>
+                                        <div className='approval-list-item-spender-wrap'>
+                                            <div className='approval-list-item-spender-title'>Spender</div>
+                                            <div className='approval-list-item-spender-content'>{vv.approved_contract.substring(0, 14) + '...' + vv.approved_contract.substring(vv.approved_contract.length-14)}</div>
+                                            <img src={ic_about_website} className='approval-list-item-spender-link' onClick={
+                                                () => {
+                                                    let url = `https://etherscan.io/address/${vv.approved_contract}`;
+                                                    if (v.chain_id == BscId) {
+                                                        url = `https://bscscan.com/address/${vv.approved_contract}`;
+                                                    }
+                                                    chrome.tabs.create({url});
+                                                }
+                                            }/>
+                                        </div>
+                                        <div className='approval-list-item-spender-wrap'>
+                                            <div className='approval-list-item-spender-title'>Allowance</div>
+                                            <div className='approval-list-item-spender-content'>{amountFormat(vv.approved_amount)}</div>
+                                            <img src={ic_about_hover} className='approval-list-item-spender-link' onClick={
+                                                () => {
+                                                    let url = `https://etherscan.io/address/${vv.approved_contract}`;
+                                                    if (v.chain_id == BscId) {
+                                                        url = `https://bscscan.com/address/${vv.approved_contract}`;
+                                                    }
+                                                    chrome.tabs.create({url});
+                                                }
+                                            }/>
+                                        </div>
+                                        <div className='approval-list-item-spender-wrap'>
+                                            <div className='approval-list-item-spender-title'>Time</div>
+                                            <div className='approval-list-item-spender-content'>{timeFormat(vv.approved_time)}</div>
+                                        </div>
+                                        {(ii < v.approved_list.length - 1) && (<div className='approval-list-line-16px'/>)}
+                                    </div>
+                                ))}
+                            </div>
+                        ))}
                     </div>
                 </div>
             )}
